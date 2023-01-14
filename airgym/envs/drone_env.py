6f64354ca95304761ae070f8c06e9225738a3c88
Guraashish-Kainth <99357289+Guraashish-Kainth@users.noreply.github.com>
2023-01-14 16:21:10 -0500

    Working LIDAR Input

--- airgym/envs/drone_env.py
+++ airgym/envs/drone_env.py
@@ -8,7 +8,10 @@ import gym
 from gym import spaces
 from airgym.envs.airsim_env import AirSimEnv
 import time
-
+from metric_gui import MetricsGui
+from PyQt5.QtWidgets import QApplication
+import sys
+import pprint
 
 
 class  AirSimDroneEnvV1(AirSimEnv):
@@ -19,12 +22,14 @@ class  AirSimDroneEnvV1(AirSimEnv):
         self.negative_reward = 0
         self.threshold_start_time = time.time()
 
+
         self.state = {
             "prev_position": np.zeros(3),
             "position": np.zeros(3),
             "collision": False,
             "prev_dist": np.zeros(3),
             "curr_dist": np.zeros(3),
+            "processed_lidar": np.zeros(4),
         }
 
         self.drone = airsim.MultirotorClient(ip=ip_address)
@@ -34,6 +39,9 @@ class  AirSimDroneEnvV1(AirSimEnv):
         self.image_request = airsim.ImageRequest(
             3, airsim.ImageType.DepthPerspective, True, False
         )
+        #self.app = QApplication(sys.argv)
+        #sys.exit(self.app.exec_())
+        #self.metricsGUI = MetricsGui()
 
     def __del__(self):
         self.drone.reset()
@@ -70,6 +78,81 @@ class  AirSimDroneEnvV1(AirSimEnv):
 
     def get_dist(self, position):
         return self.get_destination() - position
+    
+    def detect_obstacle(self, box):
+        point_count =  0
+        for dist_point in box:
+            if dist_point:
+                point_count = point_count + 1
+        if point_count >= box.size/3:
+            return 1
+        else:
+            return 0
+
+    def parse_lidarData(self, data):
+
+        # reshape array of floats to array of [X,Y,Z]
+        points = np.array(data.point_cloud, dtype=np.dtype('f4'))
+        points = np.reshape(points, (int(points.shape[0]/3), 3))
+       
+        return points    
+    #Input: 2D numpy array of Point Cloud LIDAR from AirSim API with a preset threshold
+    #Funtion: Paritions the LIDAR into boxes then tests each box for threat
+    #Output: numpy array of size 9, each element indicating the presence or absence of a threat in given box
+    def process_lidar(self, lidar):
+        x_step_size = lidar.shape[1]/3
+        y_step_size = lidar.shape[0]/3
+        obstacles = np.zeros(9)
+
+        #Cutting the LIDAR into 9 boxes
+        box00 = lidar[0:x_step_size, 0:y_step_size]
+        box01 = lidar[0:x_step_size, y_step_size:y_step_size*2 + 1]
+        box02 = lidar[0:x_step_size, y_step_size*2 + 1:y_step_size*3 + 1]
+        box10 = lidar[x_step_size:x_step_size*2 + 1, 0:y_step_size]
+        box11 = lidar[x_step_size:x_step_size*2 + 1, y_step_size:y_step_size*2 + 1]
+        box12 = lidar[x_step_size:x_step_size*2 + 1, y_step_size*2 + 1:y_step_size*3 + 1]
+        box20 = lidar[x_step_size*2 + 1:x_step_size*3 + 1, 0:y_step_size]
+        box21 = lidar[x_step_size*2 + 1:x_step_size*3 + 1, y_step_size:y_step_size*2 + 1]
+        box22 = lidar[x_step_size*2 + 1:x_step_size*3 + 1, y_step_size:y_step_size*3 + 1]
+
+        #Check each individual LIDAR box for obstacle
+        obstacles[0] = self.detect_obstacle(box00) 
+        obstacles[1] = self.detect_obstacle(box01) 
+        obstacles[2] = self.detect_obstacle(box02) 
+        
+        obstacles[3] = self.detect_obstacle(box10) 
+        obstacles[4] = self.detect_obstacle(box11) 
+        obstacles[5] = self.detect_obstacle(box12) 
+        
+        obstacles[6] = self.detect_obstacle(box20) 
+        obstacles[7] = self.detect_obstacle(box21) 
+        obstacles[8] = self.detect_obstacle(box22) 
+
+        return obstacles
+    
+    def lidar_processing(self):
+        lidar_results = np.zeros(4)
+        lidarData = self.drone.getLidarData(lidar_name="LidarSensor1", vehicle_name= "SimpleFlight")
+        if(len(lidarData.point_cloud)):
+            lidar_results[0] = 1
+        else:
+            lidar_results[0] = 0
+        lidarData = self.drone.getLidarData(lidar_name="LidarSensor2", vehicle_name= "SimpleFlight")
+        if(len(lidarData.point_cloud)):
+            lidar_results[1] = 1
+        else:
+            lidar_results[1] = 0    
+        lidarData = self.drone.getLidarData(lidar_name="LidarSensor3", vehicle_name= "SimpleFlight")
+        if(len(lidarData.point_cloud)):
+            lidar_results[2] = 1
+        else:
+            lidar_results[2] = 0
+        lidarData = self.drone.getLidarData(lidar_name="LidarSensor4", vehicle_name= "SimpleFlight")
+        if(len(lidarData.point_cloud)):
+            lidar_results[3] = 1
+        else:
+            lidar_results[3] = 0
+        self.state["processed_lidar"] = lidar_results
 
     # pretty much just the current state of the drone the img, prev position, velocity, prev dist, curr dist, collision
     def _get_obs(self):
@@ -88,6 +171,8 @@ class  AirSimDroneEnvV1(AirSimEnv):
         collision = self.drone.simGetCollisionInfo().has_collided
         self.state["collision"] = collision
 
+        #self.state["processed_lidar"] = self.process_lidar()
+
         return image
 
     # the actual movement of the drone
@@ -100,7 +185,11 @@ class  AirSimDroneEnvV1(AirSimEnv):
             quad_vel.z_val + quad_offset[2] * 10,
             5,
         ).join()
-        #print(self.state["position"]) # debug 
+        
+        if(self.drone.getLidarData().point_cloud):
+            self.state["processed_lidar"] = 1
+        else:
+            self.state["processed_lidar"] = 0
 
     def calc_dist(self, pointA, pointB):
         return math.sqrt(pow(pointA.x_val - pointB.x_val, 2) + pow(pointA.y_val - pointB.y_val, 2) + pow(pointA.z_val - pointB.z_val, 2))
@@ -192,6 +281,7 @@ class  AirSimDroneEnvV1(AirSimEnv):
         obs = self._get_obs()
         reward, done = self._compute_reward()
 
+        #self.metricsGUI.refresh()
         return obs, reward, done, self.state
 
     def reset(self):
