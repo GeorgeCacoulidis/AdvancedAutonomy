3c1e3f550a3766d4e6f9537d1ff1bc50a47de7f1
sam <vanduynsam@gmail.com>
2023-02-09 19:16:15 -0500

    police car object detection

---
+++ testing.py
@@ -0,0 +1,26 @@
+import torch
+import pandas
+
+model = torch.hub.load('ultralytics/yolov5', 'custom', 'police_model')
+
+img = 'tests/sktSEhIa8q.jpg'
+
+results = model(img, size = 1600)
+
+
+ambulance_found = False
+x_min = -1
+x_max = -1
+y_min = -1
+y_max = -1
+for box in results.xyxy[0]: 
+    if box[5]==0:
+        ambulance_found = True
+        x_min = float(box[0])
+        y_min = float(box[1])
+        x_max = float(box[2])
+        y_max = float(box[3])
+
+print(ambulance_found, x_min, x_max, y_min, y_max)
+results.print()
+print(results.pandas().xyxy[0])
