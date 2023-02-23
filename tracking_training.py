9eb7ef8f5d6261583fe4e6f6baf26dba42d4ef5c
George Cacoulidis <ge0rgec@protonmail.com>
2023-02-23 01:11:59 -0500

    updated tracking_training.py script

--- tracking_training.py
+++ tracking_training.py
@@ -3,7 +3,6 @@ import gym
 import airgym
 import time
 
-#from stable_baselines3 import DQN
 from stable_baselines3 import DQN
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.vec_env import DummyVecEnv, VecTransposeImage
@@ -25,7 +24,8 @@ env = DummyVecEnv(
 )
 
 # Wrap env as VecTransposeImage to allow SB to handle frame observations
-#lenv = VecTransposeImage(env)
+#env = VecTransposeImage(env)
+
 # Initialize RL algorithm type and parametersll
 model = DQN(
     "MlpPolicy",
@@ -41,8 +41,8 @@ model = DQN(
     max_grad_norm=10,
     exploration_fraction=0.1,
     exploration_final_eps=0.01,
-    device="cuda",
-    tensorboard_log="./tb_logs/"
+    device="auto",
+    tensorboard_log="./tracking_tb_logs/"
 )
 
 # Create an evaluation callback with the same env, called every 10000 iterations
@@ -51,9 +51,9 @@ eval_callback = EvalCallback(
     env,
     callback_on_new_best=None,
     n_eval_episodes=5,
-    best_model_save_path=f"./best_models/{str(time.time())}",
-    log_path=f"./eval_logs/{str(time.time())}",
-    eval_freq=10000,
+    best_model_save_path=f"./tracking_training_best_model",
+    log_path=f"./tracking_training_eval_logs",
+    eval_freq=5000,
 )
 callbacks.append(eval_callback)
 
@@ -66,10 +66,10 @@ kwargs["callback"] = callbacks
 
 # Train for a certain number of timesteps
 model.learn(
-    total_timesteps=5e6,
-    tb_log_name="tracking_training" + str(time.time()),
+    total_timesteps=1e5,
+    tb_log_name="tracking_training_tb_logs",
     **kwargs
 )
 
 # Save policy weights
-model.save("tracking_training")
+model.save("tracking_training_final_save")
