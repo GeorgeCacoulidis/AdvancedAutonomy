03d0db013c14a5642398accfce79dbe136bbaadb
George Cacoulidis <ge0rgec@protonmail.com>
2023-03-24 18:26:31 -0400

    tracking ppo script depr updated

--- tracking_ppo_depr.py
+++ tracking_ppo_depr.py
@@ -20,7 +20,7 @@ env = DummyVecEnv(
                 "airsim-car-tracking-v1",
                 ip_address="127.0.0.1",
                 step_length=1,
-                image_shape=(5,),
+                image_shape=(19,),
             )
         )
     ]
@@ -30,21 +30,22 @@ env = DummyVecEnv(
 model = PPO(
     "MlpPolicy",
     env,
-    learning_rate=linear_schedule(0.1),
-    n_steps=2048,
+    learning_rate=.001,
+    n_steps=1024,
     verbose=1,
-    batch_size=64,
+    batch_size=32,
     n_epochs=10,
     gamma=0.99,
     gae_lambda=0.95,
     clip_range=0.2,
     clip_range_vf=None,
-    ent_coef=0.0,
+    ent_coef=0.01,
     vf_coef=0.5,
     max_grad_norm=0.5,
+    normalize_advantage=False,
     use_sde=False,
     sde_sample_freq=-1,
-    target_kl=None,
+    target_kl=.3,
     device="cuda",
     tensorboard_log=f"{save_dir}/tb_logs/",
     _init_setup_model=True
@@ -56,32 +57,31 @@ eval_callback = EvalCallback(
     env,
     callback_on_new_best=None,
     n_eval_episodes=5,
-    best_model_save_path=f"{save_dir}/best_model",
-    log_path=f"{save_dir}/logs/",
+    best_model_save_path=save_dir,
+    log_path=save_dir,
     eval_freq=5000,
 )
 callbacks.append(eval_callback)
 
-progress_bar_callback = ProgressBarCallback()
-callbacks.append(progress_bar_callback)
-
-# Add a checkpoint callback 
+# Add a checkpoint callback to save the model and buffer every N steps
 checkpoint_callback = CheckpointCallback(
-    save_freq=500, 
-    save_path=f'{save_dir}/checkpoint_logs/',
-    save_vecnormalize=True
+    save_freq=1000, 
+    save_path=f"{save_dir}/checkpoint_save",
 )
 callbacks.append(checkpoint_callback)
 
+# Create a progress bar callback to estimate time left
+progress_bar_callback = ProgressBarCallback()
+callbacks.append(progress_bar_callback)
 kwargs = {}
 kwargs["callback"] = callbacks
 
 # Train for a certain number of timesteps
 model.learn(
-    total_timesteps=1e5,
-    tb_log_name="ppo_updated_bounding_box",
+    total_timesteps=1e6,
+    tb_log_name=f"{save_dir}",
     **kwargs
 )
 
 # Save policy weights
-model.save(f"{save_dir}/ppo_updated_bounding_box")
+model.save(f"{save_dir}/final_save")
